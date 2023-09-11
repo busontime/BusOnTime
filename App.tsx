516c88e7feac7924b998d9ad17e2148f2ea0532b
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-09-11 02:34:26 -0500

    feat: :sparkles: update app component | router | providers

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { SafeAreaView, StatusBar, LogBox, Text } from 'react-native';
+import { SafeAreaView, StatusBar, LogBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Providers } from '@/contexts';
+import { AppRouter } from '@/routes';
 
 LogBox.ignoreLogs(['']);
 // LogBox.ignoreAllLogs();
 
 export const App = (): JSX.Element => {
-  const title = 'Bienvenidos a mi app Bus on Time';
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <Text>{title}</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Providers>
+        <SafeAreaView style={{ flex: 1 }}>
+          <StatusBar animated={true} />
+          <AppRouter />
+        </SafeAreaView>
+      </Providers>
+    </NavigationContainer>
   );
 };
