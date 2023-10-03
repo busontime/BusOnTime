8422ad95a37525b17dcb88cdef7f1b19e4de66cb
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-10-03 16:10:12 -0500

    feat: :sparkles: add constants

---
+++ src/constants/bd.ts
@@ -0,0 +1,12 @@
+export const ROLES_ID = {
+  passenger: 'DVVofmbngv1LpKhXr9R6',
+  driver: 'd27oMBeRlEfaoETWk7JZ',
+  admin: 'j4mjBDAjgpASG4ztWoM3',
+};
+
+export const ROUTERS = {
+  passenger: 'passenger',
+  driver: 'driver',
+  admin: 'admin',
+  public: 'public',
+};
---
+++ src/constants/styles.ts
@@ -0,0 +1,6 @@
+export const COLORS = {
+  primary: '#87CEFA',
+  secondary: '#1E90FF',
+  dark: '#000',
+  light: '#fff',
+};
