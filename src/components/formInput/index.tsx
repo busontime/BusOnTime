70739c729e8985e68dcd83a786e02451ebc6c5a3
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-10-03 16:13:46 -0500

    feat(component): :sparkles: add customizable input

---
+++ src/components/formInput/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Stack, Input, Button } from 'tamagui';
+
+import { Eye, EyeOff } from 'lucide-react-native';
+
+import { useThemeContext } from '@/contexts/theme';
+
+import { COLORS } from '@/constants/styles';
+
+export const FormInput = ({
+  placeholder = '',
+  value = '',
+  onChangeText = (val) => {},
+  type = 'default',
+  isSecure = false,
+}) => {
+  const { isDark } = useThemeContext();
+
+  const [showPassword, setShowPassword] = useState(isSecure);
+
+  return (
+    <Stack>
+      <Input
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        keyboardType={type}
+        secureTextEntry={showPassword}
+        w='$20'
+        focusStyle={{
+          bw: 2,
+          boc: '$blue8',
+        }}
+        pr={isSecure ? '$8' : '$4'}
+      />
+      {isSecure && (
+        <Button
+          pos='absolute'
+          right={'$3'}
+          p='$0'
+          backgroundColor='transparent'
+          onPress={() => {
+            setShowPassword(!showPassword);
+          }}
+          icon={
+            showPassword ? (
+              <EyeOff color={isDark ? COLORS.light : COLORS.dark} size={25} />
+            ) : (
+              <Eye color={isDark ? COLORS.light : COLORS.dark} size={25} />
+            )
+          }
+        />
+      )}
+    </Stack>
+  );
+};
