f0d96f613539243d1998839dfdfb88920230c5c5
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-10-18 13:04:51 -0500

    feat(component): :sparkles: update map component
    add markers, geolocalization & direcctions

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,29 +1,153 @@
-import React from 'react';
-import { View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { PermissionsAndroid } from 'react-native';
+import { YStack, XStack, Stack } from 'tamagui';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import Geolocation from '@react-native-community/geolocation';
+import Config from 'react-native-config';
+
+import { CarFront, Footprints } from 'lucide-react-native';
+
+import { useMapContext } from '@/contexts/map';
+
+import { showAlertDialog } from '@/utils/dialog';
+
+import LocationImg from '@/assets/images/location.png';
+import BusStopImg from '@/assets/images/bus_stop.png';
+
+import { COLORS } from '@/constants/styles';
 
 import { styles } from './styles';
 
 export const Map = () => {
+  const { lines, busStops, busStopSelected, setCurrentLocation, currentLocation } = useMapContext();
+
+  const [destination, setDestination] = useState(null);
+  const [directionMode, setDirectionMode] = useState('WALKING');
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permiso de ubicación',
+          message: 'La aplicación necesita acceso a su ubicación.',
+          buttonNeutral: 'Pregúntame Luego',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'Aceptar',
+        }
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Location permission granted');
+
+        Geolocation.getCurrentPosition((info) => {
+          setCurrentLocation({
+            // latitude: -0.945576,
+            // longitude: -80.723126,
+            latitude: info.coords.latitude,
+            longitude: info.coords.longitude,
+          });
+        });
+      } else {
+        console.log('permission denied');
+
+        setCurrentLocation({
+          latitude: -0.952515,
+          longitude: -80.744904,
+        });
+
+        showAlertDialog('Permiso de Ubicación Denegado');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    if (busStopSelected) {
+      setDestination(busStopSelected.coordinate);
+    }
+  }, [busStopSelected]);
+
   return (
-    <View style={styles.container}>
+    <YStack f={1}>
       <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: -0.946515891877903,
-          longitude: -80.72203683511071,
-          latitudeDelta: 0.001,
-          longitudeDelta: 0.001,
+        provider={PROVIDER_GOOGLE}
+        loadingEnabled
+        style={styles.f1}
+        region={{
+          latitude: currentLocation?.latitude,
+          longitude: currentLocation?.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
         }}>
         <Marker
-          coordinate={{
-            latitude: -0.946515891877903,
-            longitude: -80.72203683511071,
+          draggable
+          image={LocationImg}
+          coordinate={currentLocation}
+          title={'Mi Ubicación'}
+          onDragEnd={(direcction) => {
+            setCurrentLocation(direcction.nativeEvent.coordinate);
           }}
-          title='Mi ubicación'
-          description='Manta, Ecuador'
         />
+
+        {lines.map((item, index) => (
+          <MapViewDirections
+            key={index}
+            origin={item.origin}
+            destination={item.destination}
+            waypoints={item.stops}
+            strokeColor={item.lineColor}
+            strokeWidth={1}
+            apikey={Config.GOOGLE_MAPS_API_KEY}
+            mode={'DRIVING'}
+          />
+        ))}
+
+        {busStops.map((item, index) => (
+          <Marker key={index} coordinate={item.coordinate} title={item.name} image={BusStopImg} />
+        ))}
+
+        {currentLocation && destination && (
+          <MapViewDirections
+            origin={currentLocation}
+            destination={destination}
+            strokeColor={COLORS.secondary}
+            strokeWidth={3}
+            apikey={Config.GOOGLE_MAPS_API_KEY}
+            mode={directionMode}
+          />
+        )}
       </MapView>
-    </View>
+
+      {currentLocation && destination && (
+        <XStack pos='absolute' top={'$2.5'} right={'$2.5'} space='$2'>
+          <Stack
+            bg={directionMode === 'DRIVING' ? '$gray11Dark' : '$colorTransparent'}
+            borderRadius={'$5'}
+            padding='$1.5'
+            onPress={() => {
+              setDirectionMode('DRIVING');
+            }}>
+            <CarFront color={COLORS.dark} size={30} />
+          </Stack>
+
+          <Stack
+            bg={directionMode === 'WALKING' ? '$gray11Dark' : '$colorTransparent'}
+            padding='$1.5'
+            borderRadius={'$5'}
+            onPress={() => {
+              setDirectionMode('WALKING');
+            }}>
+            <Footprints color={COLORS.dark} size={30} />
+          </Stack>
+        </XStack>
+      )}
+    </YStack>
   );
 };
--- src/components/map/styles.ts
+++ src/components/map/styles.ts
@@ -1,10 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
+  f1: {
     flex: 1,
   },
 });
