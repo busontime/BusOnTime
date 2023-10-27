be1fe6131c4d2655515e948c4b5216a31ae95d28
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-10-26 20:53:10 -0500

    feat(screen): :sparkles: update admin home

--- src/screens/admin/home/index.tsx
+++ src/screens/admin/home/index.tsx
@@ -1,27 +1,62 @@
 import React from 'react';
-import { Button, Stack, Text } from 'tamagui';
+import { Button, YStack, H3 } from 'tamagui';
 import { useNavigation } from '@react-navigation/native';
 
-import { User2 } from 'lucide-react-native';
+import { BusFront, User, Bus, TrainTrack } from 'lucide-react-native';
+
+import { useAuthContext } from '@/contexts/auth';
 
 export const AdminHomeScreen = () => {
   const navigation = useNavigation();
+  const { profile } = useAuthContext();
 
   return (
-    <Stack bg={'$backgroundFocus'} f={1} jc='center'>
-      <Text ta='center' color={'$color'}>
-        Home Admin
-      </Text>
+    <YStack bg={'$backgroundFocus'} f={1} jc='center' ai='center' space='$5'>
+      <H3 ta='center' color={'$color'}>
+        Bienvenido {profile?.person?.name}
+      </H3>
 
       <Button
-        icon={<User2 size={48} />}
+        w={'$15'}
+        size={'$5'}
+        icon={<User size={40} />}
         variant='outlined'
-        backgroundColor='$green8'
+        backgroundColor='$blue8'
         onPress={() => {
-          navigation.navigate('profile-menu' as never);
+          navigation.navigate('driver-menu' as never);
         }}>
-        Profile
+        Conductores
       </Button>
-    </Stack>
+
+      <Button
+        w={'$15'}
+        size={'$5'}
+        icon={<Bus size={40} />}
+        variant='outlined'
+        backgroundColor='$blue6'
+        onPress={() => {}}>
+        Buses
+      </Button>
+
+      <Button
+        w={'$15'}
+        size={'$5'}
+        icon={<TrainTrack size={40} />}
+        variant='outlined'
+        backgroundColor='$blue8'
+        onPress={() => {}}>
+        Lineas
+      </Button>
+
+      <Button
+        w={'$15'}
+        size={'$5'}
+        icon={<BusFront size={40} />}
+        variant='outlined'
+        backgroundColor='$blue6'
+        onPress={() => {}}>
+        Paradas
+      </Button>
+    </YStack>
   );
 };
