77dfd3a3dd927d18b4a75a46f3ecf6c21b29bb80
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-11-15 15:21:08 -0500

    feat(component): :sparkles: add new components | admin module
    add formgroup, formselect, formbuttons, modalbuttons, modaloptions, pick location & update input
    formt

---
+++ src/components/modalOptions/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { AlertDialog, Button, YStack, Text, XStack } from 'tamagui';
+
+export const ModalOptions = ({
+  children,
+  title = 'Está seguro de realizar esta acción?',
+  cancelColor = '$blue8',
+  secondButtonText = 'Eliminar',
+  secondButtonColor = '$red8',
+  secondButtonAction = () => {},
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialog.Trigger asChild>{children}</AlertDialog.Trigger>
+
+      <AlertDialog.Portal px='$3'>
+        <AlertDialog.Overlay
+          key='overlay'
+          animation='bouncy'
+          opacity={1}
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+        />
+        <AlertDialog.Content
+          bordered
+          elevate
+          key='content'
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          enterStyle={{ x: 0, y: 300, opacity: 1, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 300, opacity: 0, scale: 0.95 }}
+          x={0}
+          y={0}
+          scale={1}
+          opacity={1}>
+          <YStack space='$3'>
+            <Text ta='center' color={'$color'}>
+              {title}
+            </Text>
+
+            <XStack space='$3' jc='center'>
+              <AlertDialog.Cancel asChild>
+                <Button bg={cancelColor} color={'$color'}>
+                  Cancelar
+                </Button>
+              </AlertDialog.Cancel>
+
+              <AlertDialog.Action asChild onPress={secondButtonAction}>
+                <Button bg={secondButtonColor} color={'$color'}>
+                  {secondButtonText}
+                </Button>
+              </AlertDialog.Action>
+            </XStack>
+          </YStack>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog>
+  );
+};
---
+++ src/components/pickLocation/index.tsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, PermissionsAndroid } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import { XStack, YStack, SizableText } from 'tamagui';
+
+import { MapPin } from 'lucide-react-native';
+
+import { useMapContext } from '@/contexts/map';
+
+import { ModalButtons } from '../modalButtons';
+
+import { showAlertDialog } from '@/utils/dialog';
+
+import { COLORS } from '@/constants/styles';
+
+import { styles } from './styles';
+
+export const PickLocation = ({ selectLocation = (val) => {}, coordinate = null }) => {
+  const { setCurrentLocation, currentLocation } = useMapContext();
+
+  const [showModal, setShowModal] = useState(false);
+  const [marker, setMarker] = useState(null);
+
+  const handlerMapPress = (event) => {
+    const { coordinate } = event.nativeEvent;
+    setMarker(coordinate);
+  };
+
+  const closeModal = () => {
+    setMarker(null);
+    setShowModal(false);
+  };
+
+  const handlerAccept = () => {
+    if (!marker) {
+      showAlertDialog('Seleccione la ubicación de la parada!');
+      return;
+    }
+
+    selectLocation(marker);
+    closeModal();
+  };
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permiso de ubicación',
+          message: 'La aplicación necesita acceso a su ubicación.',
+          buttonNeutral: 'Pregúntame Luego',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'Aceptar',
+        }
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Location permission granted');
+
+        Geolocation.getCurrentPosition((info) => {
+          setCurrentLocation({
+            // latitude: -0.945576,
+            // longitude: -80.723126,
+            latitude: info.coords.latitude,
+            longitude: info.coords.longitude,
+          });
+        });
+      } else {
+        console.log('permission denied');
+
+        setCurrentLocation({
+          latitude: -0.952515,
+          longitude: -80.744904,
+        });
+
+        showAlertDialog('Permiso de Ubicación Denegado');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    if (coordinate?.latitude !== '' && coordinate?.longitude !== '') {
+      setMarker(coordinate);
+    }
+  }, [coordinate]);
+
+  return (
+    <YStack marginVertical='$2'>
+      <XStack
+        width={'$20'}
+        space={'$2.5'}
+        alignItems='center'
+        onPress={() => {
+          setShowModal(true);
+        }}>
+        <MapPin size={30} color={COLORS.secondary} strokeWidth={2} />
+
+        <SizableText color={'$color'} fontWeight={'bold'}>
+          Seleccionar la ubicación
+        </SizableText>
+      </XStack>
+
+      <Modal animationType='fade' transparent visible={showModal}>
+        <YStack f={1}>
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            loadingEnabled
+            mapType='standard'
+            minZoomLevel={18}
+            customMapStyle={styles}
+            initialRegion={{
+              latitude: marker ? marker?.latitude : currentLocation?.latitude,
+              longitude: marker ? marker?.longitude : currentLocation?.longitude,
+              latitudeDelta: 0.05,
+              longitudeDelta: 0.05,
+            }}
+            onPress={handlerMapPress}
+            style={{ flex: 1 }}>
+            {marker && <Marker title='Parada' coordinate={marker} pinColor={COLORS.secondary} />}
+          </MapView>
+
+          <ModalButtons firstButtonAction={closeModal} secondButtonAction={handlerAccept} />
+        </YStack>
+      </Modal>
+    </YStack>
+  );
+};
