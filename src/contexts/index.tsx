67e851f77366cb24aabd815f0ac70cb10503952c
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-11-20 12:42:17 -0500

    feat(context): :sparkles: add travel context

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -5,12 +5,15 @@ import { type ChildrenProps } from '@/interfaces';
 import { ThemeProvider } from './theme';
 import { AuthProvider } from './auth';
 import { MapProvider } from './map';
+import { TravelProvider } from './travel';
 
 export const Providers: React.FC<ChildrenProps> = ({ children }) => {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <MapProvider>{children}</MapProvider>
+        <MapProvider>
+          <TravelProvider>{children}</TravelProvider>
+        </MapProvider>
       </AuthProvider>
     </ThemeProvider>
   );
