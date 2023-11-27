8d3ac6ae920491282a6564a887068106f01020e8
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-11-27 16:45:47 -0500

    feat(screen\): :sparkles: update travel list | driver
    change get all by get all by id & add cancellation message

--- src/screens/driver/travel/travelList/index.tsx
+++ src/screens/driver/travel/travelList/index.tsx
@@ -2,48 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { ScrollView, Card, YStack } from 'tamagui';
 
+import { useAuthContext } from '@/contexts/auth';
+
 import { travelService } from '@/services/travel';
 
 import { HeaderList } from '@/components/admin/headerList';
 import { CardItem } from '@/components/admin/cardItem';
 
-import { convertFirestoreDateToString } from '@/utils/helpers';
-
-import { TRAVEL_STATUS } from '@/constants/bd';
-import { COLORS } from '@/constants/styles';
+import { convertFirestoreDateToString, getTravelStatus } from '@/utils/helpers';
 
 export const TravelList = () => {
   const navigation = useNavigation();
+  const { profile } = useAuthContext();
+  const { user } = profile;
 
   const [travels, setTravels] = useState([]);
 
   const getTravels = async () => {
     try {
-      const data = await travelService.getAll();
+      const data = await travelService.getAllByDriverId(user.uid);
 
       setTravels(data);
     } catch (error) {
       console.log('Error al recuperar todos los recorridos', error);
-    }
-  };
-
-  const getStatusLabel = (status) => {
-    switch (status) {
-      case TRAVEL_STATUS.active:
-        return { label: 'Activo', color: COLORS.green };
-        break;
-
-      case TRAVEL_STATUS.finalized:
-        return { label: 'Finalizado', color: COLORS.secondary };
-        break;
-
-      case TRAVEL_STATUS.cancelled:
-        return { label: 'Cancelado', color: COLORS.red };
-        break;
-
-      default:
-        return { label: '', color: COLORS.light };
-        break;
     }
   };
 
@@ -88,9 +69,9 @@ export const TravelList = () => {
             mb='$4'>
             <CardItem label='Fecha:' value={convertFirestoreDateToString(item?.date)} />
 
-            <CardItem label='Hora de Salida:' value={item?.startTime} />
+            <CardItem label='Hora de Inicio:' value={item?.startTime} />
 
-            <CardItem label='Hora de Llegada:' value={item?.endTime} />
+            <CardItem label='Hora de Fin:' value={item?.endTime} />
 
             <CardItem label='Linea:' value={item?.line?.name} />
 
@@ -98,9 +79,13 @@ export const TravelList = () => {
 
             <CardItem
               label='Estado:'
-              value={getStatusLabel(item?.state).label}
-              color={getStatusLabel(item?.state).color}
+              value={getTravelStatus(item?.state).label}
+              color={getTravelStatus(item?.state).color}
             />
+
+            {item.cancellation_message && (
+              <CardItem label='Motivo de cancelación' value={item?.cancellation_message} />
+            )}
           </Card>
         ))}
       </ScrollView>
