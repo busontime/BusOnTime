9a70f4fcaaa1a95ae3b08b9af407e5d6d1bb57dc
Rommelpalma123 <rommelpalma.3000@gmail.com>
2023-12-04 15:13:28 -0500

    feat: add splash to initial app

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { TamaguiProvider } from 'tamagui';
 import config from './tamagui.config';
+import SplashScreen from 'react-native-splash-screen';
 
 import { Providers } from '@/contexts';
 import { ConfigApp } from './src';
@@ -15,6 +16,18 @@ LogBox.ignoreLogs(['']);
 // LogBox.ignoreAllLogs();
 
 export const App = (props) => {
+  async function splash() {
+    try {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 5000);
+      });
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      SplashScreen.hide();
+    }
+  }
+  splash();
   return (
     <LoadingProvider>
       <TamaguiProvider config={config}>
