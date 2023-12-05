75fb86edb39c447e6c05072d5b48cac2d99bf017
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-05 00:01:28 -0500

    fix: :bug: resolve splash bugs

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import { TamaguiProvider } from 'tamagui';
 import config from './tamagui.config';
-import SplashScreen from 'react-native-splash-screen';
 
 import { Providers } from '@/contexts';
 import { ConfigApp } from './src';
@@ -18,7 +18,7 @@ export const App = (props) => {
   async function splash() {
     try {
       await new Promise((resolve) => {
-        setTimeout(resolve, 5000);
+        setTimeout(resolve, 2000);
       });
     } catch (e) {
       console.warn(e);
@@ -26,7 +26,9 @@ export const App = (props) => {
       SplashScreen.hide();
     }
   }
+
   splash();
+
   return (
     <TamaguiProvider config={config}>
       <NavigationContainer {...props}>
