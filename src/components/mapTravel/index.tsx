69d2cf13aa14e3611f07933c1322dbfbdf329bba
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-05 11:03:32 -0500

    feat(component): :sparkles: add map travel component

---
+++ src/components/mapTravel/index.tsx
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+import { H5 } from 'tamagui';
+import MapView, { Marker, PROVIDER_GOOGLE, Polyline } from 'react-native-maps';
+
+import { COLORS, MAP_STYLES } from '@/constants/styles';
+
+export const MapTravel = ({ travel = null }) => {
+  return (
+    <Fragment>
+      {travel && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          loadingEnabled
+          mapType='standard'
+          customMapStyle={MAP_STYLES}
+          region={{
+            latitude: travel.route ? travel?.route[0].latitude : travel?.location?.latitude,
+            longitude: travel.route ? travel?.route[0].longitude : travel?.location?.longitude,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001,
+          }}
+          style={{ flex: 1 }}>
+          {travel?.route && (
+            <Fragment>
+              <Marker coordinate={travel?.route[0]} title={'Origen'} pinColor={COLORS.primary} />
+
+              <Marker
+                coordinate={travel?.route[travel?.route.length - 1]}
+                title={'Destino'}
+                pinColor={COLORS.green}
+              />
+
+              <Polyline
+                coordinates={travel.route}
+                strokeWidth={5}
+                strokeColor={COLORS.secondary}
+                geodesic={false}
+              />
+            </Fragment>
+          )}
+        </MapView>
+      )}
+
+      {!travel && (
+        <H5 color={'$color'} ta='center' mt='$4'>
+          Recorrido Inexistente
+        </H5>
+      )}
+    </Fragment>
+  );
+};
