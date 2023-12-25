9012173289e5b76c6965af271aa1c4aa70ac1851
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-24 19:31:38 -0500

    feat: :sparkles: add search bar for bustops & lines | update home screen | passenger

---
+++ src/components/passenger/searchBar/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { XStack, Stack } from 'tamagui';
+
+import { Search } from 'lucide-react-native';
+
+import { useThemeContext } from '@/contexts/theme';
+
+import { TogleSidebar } from '@/components/togleSidebar';
+import { FormInput } from '@/components/formInput';
+
+import { COLORS } from '@/constants/styles';
+
+export const SearchBar = ({ changeSearchValue = (text) => {} }) => {
+  const { isDark } = useThemeContext();
+
+  const [search, setSearch] = useState('');
+
+  const searchLine = (text) => {
+    setSearch(text);
+    changeSearchValue(text);
+  };
+
+  return (
+    <XStack bg={'$backgroundFocus'} p='$2'>
+      <TogleSidebar />
+
+      <XStack marginLeft='$9'>
+        <FormInput placeholder='Búsqueda...' value={search} onChangeText={searchLine} w={'100%'} />
+
+        <Stack pos='absolute' right='$3' top='$2.5'>
+          <Search color={isDark ? COLORS.light : COLORS.dark} size={25} />
+        </Stack>
+      </XStack>
+    </XStack>
+  );
+};
--- src/screens/passenger/home/index.tsx
+++ src/screens/passenger/home/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { YStack } from 'tamagui';
 
-import { TogleSidebar } from '@/components/togleSidebar';
+import { SearchBar } from '@/components/passenger/searchBar';
 import { Map } from '@/components/map';
 import { BusStopView } from '@/components/passenger/busStopView';
 import { LineView } from '@/components/passenger/lineView';
@@ -9,16 +9,21 @@ import { TabBar } from '@/components/passenger/tabBar';
 
 export const PassengerHomeScreen = () => {
   const [principalTab, setPrincipalTab] = useState(true);
+  const [search, setSearch] = useState('');
 
   return (
     <YStack f={1} bg={'$backgroundFocus'}>
-      <TogleSidebar disableTheme />
+      <SearchBar changeSearchValue={setSearch} />
 
-      <Map />
+      {principalTab && (
+        <Fragment>
+          <Map />
 
-      {principalTab && <BusStopView />}
+          <BusStopView searchValue={search} />
+        </Fragment>
+      )}
 
-      {!principalTab && <LineView />}
+      {!principalTab && <LineView searchValue={search} />}
 
       <TabBar principalTab={principalTab} setPrincipalTab={setPrincipalTab} />
     </YStack>
