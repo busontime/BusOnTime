f1229d7be83f7b9d72718f7b792cacd239b9559a
Alex Santacruz <alexdavid_95_@hotmail.com>
2024-01-13 19:34:59 -0500

    feat: :sparkles: update logo component

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Circle } from 'tamagui';
+import { Image } from 'react-native';
 
-import { Bus } from 'lucide-react-native';
-
-import { useThemeContext } from '@/contexts/theme';
-
-import { COLORS } from '@/constants/styles';
+import LogoImg from '@/assets/images/logo.jpg';
 
 export const Logo = () => {
-  const { isDark } = useThemeContext();
-
   return (
-    <Circle bg={isDark ? '$blue8' : COLORS.light} p='$2.5'>
-      <Bus color={isDark ? COLORS.dark : COLORS.secondary} size={60} absoluteStrokeWidth />
-    </Circle>
+    <Image
+      style={{
+        width: 120,
+        height: 120,
+        resizeMode: 'contain',
+        borderRadius: 100,
+      }}
+      source={LogoImg}
+    />
   );
 };
